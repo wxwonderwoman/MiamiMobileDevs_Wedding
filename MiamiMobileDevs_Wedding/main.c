df5e4290761ea09d89636d257b8e946e2c969c1f
Natacha Galindo <natichuy24@Natachas-MacBook-Pro.local>
2015-09-15 15:29:49 -0400

    Initial Commit

---
+++ MiamiMobileDevs_Wedding/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MiamiMobileDevs_Wedding
+//
+//  Created by Natacha Galindo on 9/15/15.
+//  Copyright (c) 2015 Natacha Galindo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
